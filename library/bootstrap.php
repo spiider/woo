88186c6319b97e9bb7ab1f2749159b278cc22c6c
Hélder Pereira <blackspiider.86@gmail.com>
2013-10-16 11:11:27 +0100

    removed duplicated autoload

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -34,22 +34,6 @@
 	}
 
 
-function autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require $fileName;
-}
-
-
     $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $url = urldecode($url);
     $requested = "../public".$uri;
