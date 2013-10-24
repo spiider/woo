8cff5e2161bd091ba044cdee3e9629f25e1bd285
Hélder Pereira <blackspiider.86@gmail.com>
2013-10-24 10:32:00 +0100

    Removed unnecessary comment

--- public/index.php
+++ public/index.php
@@ -2,5 +2,5 @@
 ini_set('session.cookie_domain',
     substr($_SERVER['SERVER_NAME'],strpos($_SERVER['SERVER_NAME'],"."),100));
 session_start();
-//$_SESSION['email'] = "eu@ibt.pt";
+
 require_once('../library/bootstrap.php');
